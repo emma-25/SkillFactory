e578159793e63ab4db3c78143619f0b0a2d48510
emma-25 <mombellitic@gmail.com>
2022-11-02 07:40:47 -0300

    modified styles the cards and edit RickMorty

--- javascript/rickMorty.js
+++ javascript/rickMorty.js
@@ -5,11 +5,13 @@ const responseAPIAxios = users.then((response) => {
   const results = response.data.results;
   let body = ``;
   results.map((character) => {
-    const { name, image } = character;
+    const { name, image, species, location} = character;
     body += `
         <div class="cards__characters">
             <img src="${image}" class="card-image">
             <p class="card-name">${name}</p>
+            <p class="paragraph">${species}</p>
+            <p class="paragraph">${location.name}</p>
         </div>
         `;
     document.getElementById("characters").innerHTML = body;
